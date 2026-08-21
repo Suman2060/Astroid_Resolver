e9572480fb956ab508e49ce4734cb1f786f4032c
Suman Dangol <sumandangol2060@gmail.com>
2026-08-21 16:19:54 +0545

    feat: add latency time in mockapi

--- web/src/api/mockApi.ts
+++ web/src/api/mockApi.ts
@@ -1,9 +1,27 @@
 import type { Resources } from "../../../shared/types";
 import { resources } from "../../../shared/seed";
 
+
+function Delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function randomDelay() {
+  return Math.floor(Math.random() * 1001) + 500;
+
+}
+
+function randomFail() {
+  if (Math.random() < 0.1) {
+    alert("Error caused randomly")
+    console.log("Error cause by random failure")
+    throw new Error("System Failed..")
+    
+  }
+}
+
 export async function getResources(): Promise<Resources[]> {
-
-  await new Promise((resolve) => setTimeout(resolve,800));
-
-  return resources;
+  await Delay(randomDelay())
+  randomFail()
+  return resources
 }
